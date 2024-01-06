9e43f8272aef25e4e5c9cb9059eedf8de88f9617
arafatGithubCode <md489288@gmail.com>
2024-01-06 19:21:42 +0600

    completing about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,27 @@
 const About = () => {
   return (
-    <section>
-      <h1>About</h1>
+    <section className="px-3 py-20 space-y-6 max-w-6xl mx-auto">
+      <h1 className="text-slate-700 text-3xl font-bold">About Arafat Estate</h1>
+      <p>
+        Arafat Estate is a leading real estate agency that specializes in
+        helping clients buy, sell, and rent properties in the most desirable
+        neighborhoods. Our team of experienced agents is dedicated to providing
+        exceptional service and making the buying and selling process as smooth
+        as possible.
+      </p>
+      <p>
+        Our mission is to help our clients achieve their real estate goals by
+        providing expert advice, personalized service, and a deep understanding
+        of the local market. Whether you are looking to buy, sell, or rent a
+        property, we are here to help you every step of the way.
+      </p>
+      <p>
+        Our team of agents has a wealth of experience and knowledge in the real
+        estate industry, and we are committed to providing the highest level of
+        service to our clients. We believe that buying or selling a property
+        should be an exciting and rewarding experience, and we are dedicated to
+        making that a reality for each and every one of our clients.
+      </p>
     </section>
   );
 };
